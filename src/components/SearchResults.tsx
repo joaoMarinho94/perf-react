588cbbedf0af6c40942f34cccb4a2db7529a6c2e
joaoMarinho94 <joaopaulim94@gmail.com>
2022-02-17 23:43:15 -0300

    feat: add virtualized list

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from './ProductItem';
 
 interface SearchResultsProps {
@@ -16,17 +18,24 @@ export function SearchResults({
   totalPrice,
   onAddToWishList,
 }: SearchResultsProps): JSX.Element {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => (
+    <div key={key} style={style}>
+      <ProductItem product={results[index]} onAddToWishList={onAddToWishList} />
+    </div>
+  );
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results?.map(product => (
-        <ProductItem
-          key={product.id}
-          product={product}
-          onAddToWishList={onAddToWishList}
-        />
-      ))}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results?.length || 0}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
